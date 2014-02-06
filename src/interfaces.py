41e763fac090d55b7482286456d332482c0735b5
diutsu <sousago@gmail.com>
2014-02-06 18:16:44 +0000

    Enables network interfaces edition

---
+++ src/interfaces.py
@@ -0,0 +1,49 @@
+import re
+import os
+
+def setStatic(settings) :
+    fwrite = open('/etc/network/interfaces~','w')
+    with open('/etc/network/interfaces','r') as fread:
+        try:
+            for line in fread :
+                line_out = re.sub(r"(iface .*eth0 .*inet.*) dhcp",r"\g<1> static",line)
+                if line != line_out :
+                    fwrite.write(line_out)    
+                    for key in settings.keys() :
+                        line = "\t" + key + " " + settings[key] + "\n"
+                        fwrite.write(line)
+                    continue
+                fwrite.write(line)
+        finally:
+            fread.close()
+            fwrite.close()
+            os.rename('/etc/network/interfaces~','/etc/network/interfaces')
+
+def setDHCP():
+    fwrite = open('/etc/network/interfaces~','w')
+    with open('/etc/network/interfaces','r') as fread:
+        try:
+            replaced = False
+            for line in fread :
+                line_out = re.sub(r"(iface .*eth0 .*inet.*) static",r"\g<1> dhcp",line)
+                if line != line_out :
+                    replaced = True
+                    fwrite.write(line_out)
+                    continue
+                if replaced :
+                    if ("address" in line or "network" in line or
+                        "broadcast" in line or "gateway" in line or
+                        "netmask" in line):
+                        continue
+                replaced = False
+                fwrite.write(line)
+        finally:
+            fread.close()
+            fwrite.close()
+            os.rename('/etc/network/interfaces~','/etc/network/interfaces')
+
+
+
+st = { "address" : "1.1.1.1", "network":"1.0.0.0", "broadcast" : "0.1.1.1",
+        "gateway" : "1.0.1.0", "netmask":"1.0.0.1"}
+setStatic(st)
